6e0e86e187f72a36accbbcf26304168a5a60d99c
Gustavo Corone <gcoronelc@gmail.com>
2022-08-29 09:52:41 -0500

    Semana 1

--- Semana01/Work1.sql
+++ Semana01/Work1.sql
@@ -16,3 +16,10 @@ select d.iddepartamento, d.nombre, e.idempleado, e.nombre, e.apellido
 from rh..empleado e
 join rh..departamento d on e.iddepartamento=d.iddepartamento;
 go
+
+
+select d.iddepartamento, d.nombre, avg(e.sueldo) "Sueldo Promedio"
+from rh..empleado e
+join rh..departamento d on e.iddepartamento=d.iddepartamento
+group by d.iddepartamento, d.nombre;
+go
