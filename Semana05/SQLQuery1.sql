5f50d0ac628891a447492efb9a9bd1c3d1e30458
Gustavo Corone <gcoronelc@gmail.com>
2022-09-26 09:04:42 -0500

    Semana 05

---
+++ Semana05/SQLQuery1.sql
@@ -0,0 +1,79 @@
+-- Problema 1
+
+SELECT * FROM RH..empleado;
+GO
+
+SELECT 
+	D.nombre "NOMBRE DEPARTAMENTO",
+	COUNT(1) "CANTIDAD EMPLEADOS",
+	SUM(E.sueldo) "IMPORTE PLANILLA",
+	SUM(ISNULL(E.comision,0)) "COMISION",
+	SUM(SUELDO + ISNULL(E.comision,0)) "TOTAL"
+FROM RH..empleado E
+JOIN RH..departamento D ON E.iddepartamento = D.iddepartamento
+GROUP BY D.nombre;
+GO
+
+-- Problema 6
+
+SELECT *
+FROM Northwind..[Order Details] OD
+JOIN Northwind..Orders O ON OD.OrderID = O.OrderID
+GO
+
+WITH
+V1 AS (
+	SELECT 
+		O.ShipCountry "PAIS",
+		COUNT(DISTINCT O.OrderID) "CANTIDAD PEDIDOS",
+		SUM(OD.Quantity) "CANTIDAD PRODUCTOS",
+		SUM(OD.Quantity*OD.UnitPrice) "IMPORTE"
+	FROM Northwind..[Order Details] OD
+	JOIN Northwind..Orders O ON OD.OrderID = O.OrderID
+	GROUP BY O.ShipCountry),
+V2 AS (SELECT SUM(IMPORTE) "TOTAL" FROM V1)
+SELECT 
+	V1.*,
+	V1.IMPORTE*100.0/V2.TOTAL "PORCENTAJE"
+FROM V1 CROSS JOIN V2;
+GO
+
+SELECT COUNT(1) FROM Northwind..Orders;
+GO
+
+
+-- Problema 7
+
+SELECT 
+	O.CustomerID, OD.ProductID,
+	COUNT(DISTINCT O.OrderID) "PEDIDOS",
+	SUM(OD.Quantity) "PRODUCTOS"
+FROM Northwind..[Order Details] OD
+JOIN Northwind..Orders O ON OD.OrderID = O.OrderID
+WHERE O.CustomerID='BERGS'
+GROUP BY O.CustomerID, OD.ProductID
+ORDER BY 4 DESC
+GO
+
+WITH
+V1 AS (
+	SELECT 
+		O.CustomerID, OD.ProductID,
+		COUNT(DISTINCT O.OrderID) "PEDIDOS",
+		SUM(OD.Quantity) "PRODUCTOS"
+	FROM Northwind..[Order Details] OD
+	JOIN Northwind..Orders O ON OD.OrderID = O.OrderID
+	GROUP BY O.CustomerID, OD.ProductID),
+V2 AS (
+	SELECT CustomerID, MAX(PRODUCTOS) "PRODUCTOS MAX"
+	FROM V1
+	GROUP BY CustomerID)
+SELECT 
+	C.CompanyName, P.ProductName,
+	V1.PEDIDOS, V1.PRODUCTOS
+FROM V1 JOIN V2 
+ON V1.CustomerID=V2.CustomerID AND V1.PRODUCTOS=V2.[PRODUCTOS MAX]
+JOIN Northwind..Customers C ON V1.CustomerID=C.CustomerID 
+JOIN Northwind..Products P ON V1.ProductID=P.ProductID
+ORDER BY 1, 2
+GO
